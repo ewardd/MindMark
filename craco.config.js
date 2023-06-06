f492a527b48dd6b77b2b70a10933c2a503877e4b
Alex Petrov <sanek-ekb@mail.ru>
2023-06-07 00:22:26 +0500

    build: configure craco and aliases

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
+};

5e5090e192674e6ce24819a1258833c257fd8fdf
Alex Petrov <sanek-ekb@mail.ru>
2023-06-09 12:13:50 +0500

    chore(Auth): make "OK" response by default

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/auth.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { JwtResponse } from 'src/auth/dto/jwt-response.dto';
@@ -12,14 +12,14 @@ export class AuthController {
 
   @Public()
   @Post('sign-in')
-  @ApiResponse({ type: JwtResponse })
+  @ApiOkResponse({ type: JwtResponse })
   public signIn(@Body() signInDto: SignInDto): Promise<JwtResponse> {
     return this._authService.signIn(signInDto.email, signInDto.password);
   }
 
   @Public()
   @Post('sign-up')
-  @ApiResponse({ type: JwtResponse })
+  @ApiOkResponse({ type: JwtResponse })
   public signUp(@Body() signUpDto: SignUpDto): Promise<JwtResponse> {
     return this._authService.signUp(signUpDto);
   }

7260d55ad5d24963e712536f1e6e2103889b2156
Alex Petrov <sanek-ekb@mail.ru>
2023-06-09 10:09:02 +0500

    chore(JWT): increase exp to 7d

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,5 +1,5 @@
 export const jwtConstants = {
   // TODO: Move to ENV
   secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-  expiration: '60s',
+  expiration: '7d',
 };

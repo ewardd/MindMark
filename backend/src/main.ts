8e44c148bffe6c9084857639eb9fbf6179d2ca99
Alex Petrov <sanek-ekb@mail.ru>
2023-06-09 10:06:40 +0500

    chore: enable global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,15 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle('MindMark').setVersion('1.0').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);

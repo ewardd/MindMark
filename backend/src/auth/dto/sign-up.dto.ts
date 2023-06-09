27c582815928b3fad6f9e68138edd1cd0b9e02f4
Alex Petrov <sanek-ekb@mail.ru>
2023-06-09 12:10:02 +0500

    chore: add field validation

--- backend/src/auth/dto/sign-up.dto.ts
+++ backend/src/auth/dto/sign-up.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { User } from 'src/users/entities/user.entity';
 
 export class SignUpDto implements Partial<User> {
+  @IsEmail()
+  @IsNotEmpty()
   @ApiProperty()
   public email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   public password: string;
 }
--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,7 +1,22 @@
-import { ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { User } from 'src/users/entities/user.entity';
 
-export class UpdateUserDto extends PartialType(OmitType(User, ['passwordHash'])) {
+export class UpdateUserDto implements Partial<Omit<User, 'passwordHash' | 'createdAt' | 'updatedAt'>> {
+  @IsUUID()
+  @IsNotEmpty()
+  @ApiProperty()
+  public id: string;
+
+  @IsEmail()
+  @ApiPropertyOptional()
+  public email?: string;
+
+  @IsBoolean()
+  @ApiPropertyOptional()
+  public isActive?: boolean;
+
+  @IsString()
   @ApiPropertyOptional()
   public password?: string;
 }

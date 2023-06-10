3f983739c9f85d4a7e67b1f59744d55a0c6d2bbf
Alex Petrov <sanek-ekb@mail.ru>
2023-06-10 17:07:29 +0500

    build(Swagger): keep token on refresh

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('MindMark').setVersion('1.0').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, { swaggerOptions: { persistAuthorization: true } });
 
   await app.listen(4041);
 

41fa5a921c73e6dc4200c50ad8e501f0443661ae
Alex Petrov <sanek-ekb@mail.ru>
2023-06-12 04:38:25 +0500

    chore: add `useLogout` hook that resets the `api` store

--- webapp/src/shared/hooks/useLogout.ts
+++ webapp/src/shared/hooks/useLogout.ts
@@ -1,4 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { baseApi } from '@shared/api';
 
 /**
  * Logout action. Should be used in every slice to reset the state to initial.
@@ -10,3 +13,14 @@ import { createAction } from '@reduxjs/toolkit';
  * });
  */
 export const logout = createAction('LOGOUT_RESET');
+
+export const useLogout = () => {
+  const dispatch = useDispatch();
+
+  const onLogout = useCallback(() => {
+    dispatch(logout());
+    dispatch(baseApi.util.resetApiState());
+  }, [dispatch]);
+
+  return onLogout;
+};

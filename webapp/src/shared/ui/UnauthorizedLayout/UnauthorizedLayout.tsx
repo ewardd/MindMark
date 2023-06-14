0ce422771210ac16a38622d5737df5f697aeea0e
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 02:45:59 +0500

    chore(styles): add dark-mode bg for all layouts

--- webapp/src/shared/ui/UnauthorizedLayout/UnauthorizedLayout.tsx
+++ webapp/src/shared/ui/UnauthorizedLayout/UnauthorizedLayout.tsx
@@ -8,8 +8,8 @@ export const UnauthorizedLayout: React.FC<IUnauthorizedLayoutProps> = (props) =>
   const { children } = props;
 
   return (
-    <div className={'flex justify-center items-center min-w-full h-screen'}>
-      <div className={'border border-gray-400 shadow-md border-solid rounded-xl p-4'}>{children}</div>
+    <div className={'common-bg flex h-screen min-w-full items-center justify-center'}>
+      <div className={'rounded-xl border border-solid border-gray-400 p-4 shadow-md'}>{children}</div>
     </div>
   );
 };

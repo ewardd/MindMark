28655afece95fda8fd8f3eea268396b142242c9e
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 03:57:56 +0500

    chore(Header): add api usage example

--- webapp/src/widgets/Header/Header.tsx
+++ webapp/src/widgets/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Layout, Space } from 'antd';
 import React from 'react';
 import { ToggleTheme } from '@features/ToggleTheme';
-import { IUserProfileProps, UserProfile } from '@entities/User';
+import { IUserProfileProps, useMeQuery, UserProfile } from '@entities/User';
 import { useLogout } from '@shared/hooks';
 
 interface IBaseHeaderProps {
@@ -20,10 +20,12 @@ export const BaseHeader: React.FC<IBaseHeaderProps> = () => {
     },
   ];
 
+  const { data } = useMeQuery();
+
   return (
     <Layout.Header className={'layout-bg flex flex-row justify-end'}>
       <Space direction={'horizontal'} size={'large'}>
-        <UserProfile items={items} userName={'Eward'} />
+        {!!data && <UserProfile items={items} userName={data?.email} />}
 
         <ToggleTheme />
       </Space>

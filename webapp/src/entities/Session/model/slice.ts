38b671448f816b1dfbc87173bea8a4945fd9fca5
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 05:26:15 +0500

    chore(auth): add `refreshToken`

--- webapp/src/entities/Session/model/slice.ts
+++ webapp/src/entities/Session/model/slice.ts
@@ -3,36 +3,36 @@ import { setAuthTokens } from '@shared/api';
 import { logout } from '@shared/hooks';
 
 const LOCAL_STORAGE_KEY = 'token';
+const LOCAL_STORAGE_REFRESH_KEY = 'token_refresh';
 
 interface ISessionState {
   accessToken: string | null;
+  refreshToken: string | null;
 }
 
 const initialState: ISessionState = {
   accessToken: localStorage.getItem(LOCAL_STORAGE_KEY),
+  refreshToken: localStorage.getItem(LOCAL_STORAGE_REFRESH_KEY),
 };
 
 export const sessionSlice = createSlice({
   initialState,
   name: 'sessionSlice',
-  reducers: {
-    setAccessToken: (state, action: PayloadAction<string>) => {
-      localStorage.setItem(LOCAL_STORAGE_KEY, action.payload);
-      state.accessToken = action.payload;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) =>
     builder
       .addCase(setAuthTokens, (state, action) => {
         localStorage.setItem(LOCAL_STORAGE_KEY, action.payload.accessToken);
+        localStorage.setItem(LOCAL_STORAGE_REFRESH_KEY, action.payload.refreshToken);
         state.accessToken = action.payload.accessToken;
+        state.refreshToken = action.payload.refreshToken;
       })
       .addCase(logout, (state) => {
-      localStorage.removeItem(LOCAL_STORAGE_KEY);
-      state.accessToken = null;
-    }),
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        localStorage.removeItem(LOCAL_STORAGE_REFRESH_KEY);
+        state.accessToken = null;
+        state.refreshToken = null;
+      }),
 });
 
 export default sessionSlice.reducer;
-
-export const { setAccessToken } = sessionSlice.actions;

5de6b89e20932c997df61959ec3f6f30c197b85c
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 06:07:00 +0500

    chore: add `isTokensResult` type-guard

--- webapp/src/shared/api/setAuthTokensAction.ts
+++ webapp/src/shared/api/setAuthTokensAction.ts
@@ -2,3 +2,11 @@ import { createAction } from '@reduxjs/toolkit';
 import { ISession } from '@shared/api';
 
 export const setAuthTokens = createAction<ISession>('SET_AUTH_TOKENS');
+
+export const isTokensResult = (data: unknown): data is ISession => {
+  if (!data) return false;
+  if (typeof data !== 'object') return false;
+  if (!('accessToken' in data && 'refreshToken' in data)) return false;
+
+  return true;
+};

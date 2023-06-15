bbb0330f9da2cf66b8962365867e428bd881e178
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 06:08:16 +0500

    chore(baseQuery): add `refreshToken` header handler

--- webapp/src/shared/api/baseQuery.ts
+++ webapp/src/shared/api/baseQuery.ts
@@ -1,17 +1,25 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '@shared/hooks';
 
+export const REFRESH_TOKEN_PLACEHOLDER = '_REFRESH_TOKEN_';
+
 const BASE_URL = process.env.REACT_APP_SERVER_ENDPOINT as string;
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: BASE_URL,
-    prepareHeaders: (headers, { getState }) => {
-      const { accessToken } = (getState() as RootState).sessionState;
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers, { getState }) => {
+    if (headers.get('Authorization') === REFRESH_TOKEN_PLACEHOLDER) {
+      const { refreshToken } = (getState() as RootState).sessionState;
 
-      if (accessToken) {
-        headers.set('Authorization', `Bearer ${accessToken}`);
-      }
-
+      if (refreshToken) headers.set('Authorization', `Bearer ${refreshToken}`);
+      else headers.delete('Authorization');
       return headers;
-    },
-  });
+    }
+
+    const { accessToken } = (getState() as RootState).sessionState;
+
+    if (accessToken) headers.set('Authorization', `Bearer ${accessToken}`);
+
+    return headers;
+  },
+});

839e591059e07de3e62f35ee22c1c380f57ca209
Alex Petrov <sanek-ekb@mail.ru>
2023-06-15 06:07:35 +0500

    ref: simplify types

--- webapp/src/shared/api/baseQuery.ts
+++ webapp/src/shared/api/baseQuery.ts
@@ -1,16 +1,9 @@
-import { type BaseQueryFn } from '@reduxjs/toolkit/dist/query/baseQueryTypes';
-import {
-  type FetchArgs,
-  type FetchBaseQueryError,
-  type FetchBaseQueryMeta,
-} from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '@shared/hooks';
 
 const BASE_URL = process.env.REACT_APP_SERVER_ENDPOINT as string;
 
-export const baseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta> =
-  fetchBaseQuery({
+export const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const { accessToken } = (getState() as RootState).sessionState;

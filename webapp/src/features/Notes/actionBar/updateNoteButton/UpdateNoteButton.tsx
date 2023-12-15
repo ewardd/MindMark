0ca7c39a51e7a87ab7fd27262c37b7356ea26641
Ed <ewardd2014@gmail.com>
2023-12-15 23:31:27 +0300

    ref: move component from index.tsx

---
+++ webapp/src/features/Notes/actionBar/updateNoteButton/UpdateNoteButton.tsx
@@ -0,0 +1,21 @@
+import { Button, FormInstance } from 'antd';
+import React from 'react';
+import { useUpdateNoteMutation } from '@entities/Note';
+import { IUpdateNoteDto } from '@shared/api';
+
+interface IUpdateNoteButtonProps {
+  className?: string;
+  form: FormInstance<IUpdateNoteDto>;
+}
+
+export const UpdateNoteButton: React.FC<IUpdateNoteButtonProps> = (props) => {
+  const { form } = props;
+  const [updateNote, { isLoading }] = useUpdateNoteMutation();
+  const submit = () => updateNote(form.getFieldsValue());
+
+  return (
+    <Button onClick={submit} loading={isLoading}>
+      Submit
+    </Button>
+  );
+};

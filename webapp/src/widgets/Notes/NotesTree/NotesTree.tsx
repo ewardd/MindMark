366a5f49472ab11095974d7fa837ad25fe862619
Ed <ewardd2014@gmail.com>
2023-12-15 23:33:32 +0300

    chore: add create Note button to NotesTree

--- webapp/src/widgets/Notes/NotesTree/NotesTree.tsx
+++ webapp/src/widgets/Notes/NotesTree/NotesTree.tsx
@@ -1,9 +1,24 @@
 import { Tree } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { NotesTreeRow, useGetNotesQuery } from '@entities/Note';
 
 export const NotesTree: React.FC = () => {
   const notesTree = useGetNotesQuery();
 
-  return notesTree && <Tree titleRender={(element) => <NotesTreeRow note={element} />} treeData={notesTree.data} />;
+  return (
+    notesTree && (
+      <>
+        <Link className={'flex py-2 pl-[27px]'} to={`/note/create`}>
+          Create new Note
+        </Link>
+        <Tree
+          className={'py-1'}
+          height={400}
+          titleRender={(element) => <NotesTreeRow note={element} />}
+          treeData={notesTree.data}
+        />
+      </>
+    )
+  );
 };
